840acb15f0e05bc248132d510d507921d3940b52
Alexander <70033307+Alexanderbrandhaug@users.noreply.github.com>
2021-04-23 13:36:49 +0200

    Create HotelControllerApp.java

---
+++ Project/Booking-system/Controller/HotelControllerApp.java
@@ -0,0 +1,25 @@
+package project_reservation;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HotelControllerApp extends Application {
+
+	@Override
+	public void start(final Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Bookify");
+		primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("Reservation1.fxml"))));
+		primaryStage.show();
+		
+		
+		
+	}
+	
+
+	
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+	}
