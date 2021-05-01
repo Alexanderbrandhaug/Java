6ea38d2ded6a71034b16859e2d55a9f4ebb80db5
Alexander <70033307+Alexanderbrandhaug@users.noreply.github.com>
2021-05-01 10:01:57 +0200

    Create HotelLauncher.java

---
+++ Project/Booking-system/Controller/HotelLauncher.java
@@ -0,0 +1,14 @@
+package bookingsystem;
+
+//Maven likte ikke javaFX komponenter, så må bruke denne som main-class. 
+
+public class HotelLauncher {
+
+	
+	
+	public static void main(String[] args) {
+        HotelControllerApp.main(args);
+    }
+}
+
+
